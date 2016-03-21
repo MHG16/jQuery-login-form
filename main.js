72a773922279ad5c59aa3674655819c65e05bf90
Martin Grossmann <mhgrossmann@yahoo.com>
2016-03-21 13:47:00 -0500

    Working on checking for empty input

---
+++ main.js
@@ -0,0 +1,35 @@
+// Using jQuery, build a login form. The form should check to ensure that the username and password 
+//are filled in before checking to see if the username/password combination is in the list below.
+
+// The login form should display the following errors to the user if their * input is invalid:
+// Please enter an email address before logging in.
+// Please enter a password before logging in.
+// Your user was not found.
+// The password you entered is incorrect.
+// If the user logs in successfully they should be redirected to http://* theironyard.com
+// The email address field should not allow non-email address input.
+// The password address field should have the letters obfuscated.
+// The form should submit if the user types enter or clicks the submit button.
+// Valid username / password combinations should be:
+// aaron@theironyard.com / password123
+// admin@google.com / pandas / honeycrisp
+
+var form = $('form');
+var userName = $('.userName');
+var password = $('.password');
+
+form.on('submit', function() {
+	if(userName === '') {
+		alert('Please enter an email address before loggin in.');
+	}
+	else if (password === '') {
+		alert('Please enter a password before logging in.');
+	}
+	else {
+		alert('You entered both email address and password');
+	}
+
+});
+
+
+
